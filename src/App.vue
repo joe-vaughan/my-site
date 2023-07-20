2dc56194bfc41353cf6523afed03aaff241c6032
Joe <jbvaughan1993@gmail.com>
2023-07-20 10:40:28 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="wrapper">
+    <SiteIntro/>
+  </div>
+</template>
+
+<script>
+
+import SiteIntro from './components/SiteIntro.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SiteIntro
+  }
+}
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Lunasima:wght@400;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+* {
+  margin: 0;
+  border: 0;
+  padding: 0;
+}
+
+.wrapper {
+  margin-top: 2.5vh;
+  margin-left: 5vw;
+  margin-right: 25vw;
+}
+
+ul {
+  margin-left: 2.5vw;
+}
+
+body {
+  font-family: 'Lunasima', sans-serif;
+}
+
+h1 {
+  font-size: 3rem;
+  margin-bottom: 1rem;
+  letter-spacing: -0.1rem;
+}
+
+.prose {
+  font-size: 1.5rem;
+  letter-spacing: -0.05rem;
+  line-height: 1.5;
+  margin-bottom: 1rem;
+}
+
+.emphasis {
+  font-weight: 600;
+}
+
+a {
+  color: blue;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline
+}
+
+</style>
---
+++ src/components/SiteIntro.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="">
+  <h1>Joe Vaughan</h1>
+  <div class="prose">🚧 Beep boop, this site is under construction. But don't worry: no helmets needed.</div>
+  <div class="prose"><span class='emphasis'>About</span>: I'm a writer, editor and creative based in the UK. I work in community for <a href="https://storybook.js.org/">Storybook</a> (an open source frontend workshop) and <a href="chromatic.com">Chromatic</a> (automating tests for screens and components). I used to work in museums and my campaigns were recognized in international press (incl. <a href="https://www.theguardian.com/books/2019/oct/22/woman-reunited-with-childhood-book-in-museum-reading-the-secret-garden">The Guardian</a> and <a href="https://www.npr.org/2020/08/09/900498227/the-escapist-land-of-cottagecore-from-marie-antoinette-to-taylor-swift">NPR</a>).</div>
+  <div class="prose">In my spare time, I love reading, running, and taking acting classes. I'm learning to code and building in public!</div>
+
+  <div class="prose">
+    <span class='emphasis'>Projects I wanna work on</span>:
+    <ul>
+      <li>Some kinda app for my D&D group</li>
+      <li>My personal site 👉 👈 (built with Vue)!</li>
+    </ul>
+  </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'SiteIntro',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
